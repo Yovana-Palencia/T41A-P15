dca6263b129a44c8cac961f4c9d8bb2075d41ec7
Yovana-Palencia <174197@upslp.edu.mx>
2025-11-05 19:48:26 -0600

    Update 03_crear_una_funcion_que_calcule_el_descuento_aplicado_a_un_produto.sql

--- 03_crear_una_funcion_que_calcule_el_descuento_aplicado_a_un_produto.sql
+++ 03_crear_una_funcion_que_calcule_el_descuento_aplicado_a_un_produto.sql
@@ -1,1 +1,8 @@
+-- Función para calcular el descuento aplicado a un producto
+CREATE OR REPLACE FUNCTION calcular_descuento(precio_original NUMERIC, porcentaje_descuento NUMERIC)
+RETURNS NUMERIC AS $$
+BEGIN
+    RETURN precio_original - (precio_original * porcentaje_descuento / 100);
+END;
+$$ LANGUAGE plpgsql;
 
