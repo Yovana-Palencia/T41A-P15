fce82da239201b14ad271a76d56329e4a3daf2df
Yovana-Palencia <174197@upslp.edu.mx>
2025-11-05 19:48:47 -0600

    Update 05_crear_una_funcion_que_devuelva_los_productos_con_stock_menor_a_un_valor_dado.sql

--- 05_crear_una_funcion_que_devuelva_los_productos_con_stock_menor_a_un_valor_dado.sql
+++ 05_crear_una_funcion_que_devuelva_los_productos_con_stock_menor_a_un_valor_dado.sql
@@ -1,1 +1,11 @@
+-- Función que devuelve los productos con stock menor a un valor dado
+CREATE OR REPLACE FUNCTION filtrar_stock(cantidad_minima INT)
+RETURNS TABLE(id INT, nombre TEXT, stock INT) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT id, nombre, stock
+    FROM productos
+    WHERE stock < cantidad_minima;
+END;
+$$ LANGUAGE plpgsql;
 
