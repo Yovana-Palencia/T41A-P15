824fb16220334c5ed752fc75cf401fe147497623
Yovana-Palencia <174197@upslp.edu.mx>
2025-11-17 21:43:41 -0600

    Update 07_crear_una_funcion_que_cuente_cuantos_empleados_hay_en_un_departamento.sql

--- 07_crear_una_funcion_que_cuente_cuantos_empleados_hay_en_un_departamento.sql
+++ 07_crear_una_funcion_que_cuente_cuantos_empleados_hay_en_un_departamento.sql
@@ -1,1 +1,12 @@
+CREATE OR REPLACE FUNCTION contar_empleados(dep_id INT)
+RETURNS INT AS $$
+DECLARE
+    total INT;
+BEGIN
+    SELECT COUNT(*) INTO total
+    FROM empleados
+    WHERE departamento_id = dep_id;
 
+    RETURN total;
+END;
+$$ LANGUAGE plpgsql;
