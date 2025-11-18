18a774631973b9390a58e427a65a2d11c7b8bb8f
Yovana-Palencia <174197@upslp.edu.mx>
2025-11-17 21:45:55 -0600

    Create 08_probar_funciones.sql

---
+++ 08_probar_funciones.sql
@@ -0,0 +1,5 @@
+SELECT calcular_descuento(1000, 15);
+SELECT validar_email('usuario@mail.com');
+SELECT * FROM productos_bajo_stock(10);
+SELECT dia_semana('2025-11-17');
+SELECT contar_empleados(3);
