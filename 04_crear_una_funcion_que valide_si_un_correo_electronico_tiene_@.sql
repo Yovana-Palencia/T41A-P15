fdc544e7320f3b80a06a92fb5abb09bd8b0bf955
Yovana-Palencia <174197@upslp.edu.mx>
2025-11-17 20:59:15 -0600

    Delete 04_crear_una_funcion_que valide_si_un_correo_electronico_tiene_@.sql

--- 04_crear_una_funcion_que valide_si_un_correo_electronico_tiene_@.sql
+++
@@ -1,8 +0,0 @@
--- Función para validar si un correo contiene '@'
-CREATE OR REPLACE FUNCTION validar_correo(correo TEXT)
-RETURNS BOOLEAN AS $$
-BEGIN
-    RETURN POSITION('@' IN correo) > 0;
-END;
-$$ LANGUAGE plpgsql;
-
