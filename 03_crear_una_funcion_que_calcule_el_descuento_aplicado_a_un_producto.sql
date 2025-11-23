6edf3b5a0892f1954c24a2b07d932da63a5ee335
Yovana-Palencia <174197@upslp.edu.mx>
2025-11-22 23:08:52 -0600

    Delete 03_crear_una_funcion_que_calcule_el_descuento_aplicado_a_un_producto.sql

--- 03_crear_una_funcion_que_calcule_el_descuento_aplicado_a_un_producto.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE FUNCTION calcular_descuento(precio NUMERIC, porcentaje NUMERIC)
-RETURNS NUMERIC AS $$
-BEGIN
-    RETURN precio - (precio * porcentaje / 100);
-END;
-$$ LANGUAGE plpgsql;
